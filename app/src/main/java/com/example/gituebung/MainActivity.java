6a261a13340c42b9a9c74d4ff020607977067c4e
AICaptainAmerica <Martin1.Kocur@stud.uni-regensburg.de>
2019-07-18 09:31:08 +0200

    int a erstellt

--- app/src/main/java/com/example/gituebung/MainActivity.java
+++ app/src/main/java/com/example/gituebung/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         name = "Martin";
         zweiterName = "Martin2";
+        int a = 0;
 
     }
 }
